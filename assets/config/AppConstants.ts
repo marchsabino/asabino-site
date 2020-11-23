0c63e1c1ee46cf0434a0ef754e2d26da51287525
Marcello Sabino <marchsabino@gmail.com>
2020-11-23 15:57:32 -0500

    Auto-generated commit

---
+++ assets/config/AppConstants.ts
@@ -0,0 +1,5 @@
+export class AppConstants {
+  static readonly PHONE_NUMBER = '(203) 555-5555';
+  static readonly PHONE_NUMBER_RAW = '2035555555';
+  static readonly LICENSE_NUMBER = 'ABC.123456';
+}
